2ccca896dc31bb1234e30012eb27c11b03a14f0c
Udeh Princewill <princezubby2020@gmail.com>
2023-03-06 11:55:23 -0700

    PORT variable added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const { connectToDatabase, connection } = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 
@@ -12,10 +13,12 @@ app.get("/", (req, res) => {
 
 app.use("/users", userRoutes);
 
+const PORT_NO = process.env.PORT;
+
 connectToDatabase()
   .then(() => {
-    console.log("Server started on port 3000");
-    app.listen(3000);
+    console.log(`Server started on port ${PORT_NO}`);
+    app.listen(PORT_NO);
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error.message);
