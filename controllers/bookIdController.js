c1313389b2676e6020be5e41b3aba634a7b97731
Udeh Princewill <princezubby2020@gmail.com>
2023-03-15 15:34:09 +0100

    book by id added

--- controllers/bookIdController.js
+++ controllers/bookIdController.js
@@ -2,13 +2,19 @@ const axios = require("axios");
 
 exports.getBookById = async (req, res) => {
   const { id } = req.params;
-  console.log(id);
   const url = `https://openlibrary.org/works/${id}.json`;
-  console.log(url);
-  try {
-    const response = await axios.get(`https://openlibrary.org/works/${id}.json`);
-    const book = response.data;
 
+  try {
+    const response = await axios.get(
+      `https://openlibrary.org/works/${id}.json`
+    );
+    const storedata = response.data;
+    const book = {
+      ...storedata,
+      coverUrl: `https://covers.openlibrary.org/b/id/${storedata.covers}-M.jpg`,
+    };
+
+    
     res.status(200).json(book);
   } catch (error) {
     res.status(500).json({ error });
