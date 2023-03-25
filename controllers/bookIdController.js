0bb8fcecd9ca8882981226201220b9f225a2764d
Udeh Princewill <princezubby2020@gmail.com>
2023-03-25 23:57:31 +0100

    book id refactored

--- controllers/bookIdController.js
+++ controllers/bookIdController.js
@@ -2,7 +2,6 @@ const axios = require("axios");
 
 exports.getBookById = async (req, res) => {
   const { id } = req.params;
-  const url = `https://openlibrary.org/works/${id}.json`;
 
   try {
     const response = await axios.get(
@@ -11,9 +10,9 @@ exports.getBookById = async (req, res) => {
     const storedata = response.data;
     const book = {
       ...storedata,
+      key: storedata.key.replace("/works/", ""),
       coverUrl: `https://covers.openlibrary.org/b/id/${storedata.covers}-M.jpg`,
     };
-    console.log(book);
 
     res.status(200).json(book);
   } catch (error) {
